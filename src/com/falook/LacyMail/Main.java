90d0874a39963a8d3869cdac82f50943ef09aa04
Falook Glico <falook.glico@gmail.com>
2015-09-20 10:49:05 +0700

    Initial commit

---
+++ src/com/falook/LacyMail/Main.java
@@ -0,0 +1,49 @@
+package com.falook.LacyMail;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        final Future future = es.submit(() -> {
+            final String username = "username";
+            final String password = "password";
+
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "mailtrap.io");
+            props.put("mail.smtp.port", "2525");
+
+            Session session = Session.getInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from-email@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("to-email@gmail.com"));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+
+            return null;
+        });
+
+        System.out.println("Done");
+    }
+}
